d9ea09cc060f5aaf95ecbe67f1aff4a9c7710f6c
Priyanka200526 <priyankasaini9326@gmail.com>
2024-10-10 17:45:51 +0530

    Add files via upload

---
+++ myclass/pri.js
@@ -0,0 +1,25 @@
+const images = [
+    'image1.jpg',
+    'image2.jpg',
+    'image3.jpg',
+    'image4.jpg',
+    'image5.jpg',
+    'image6.jpg'
+];
+
+let currentIndex = 0;
+
+function showImage(index) {
+    const img = document.getElementById('mainImage');
+    img.src = images[index];
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+}
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+}
